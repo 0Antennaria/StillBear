9b46d679a46c538da62b922f315a215c7c380bb4
antennaria <antennaria.arktik@gmail.com>
2024-11-23 09:00:26 +0300

    Add Animation

--- StillBear/Assets/Scripts/InputSystem/CharacterInputController.cs
+++ StillBear/Assets/Scripts/InputSystem/CharacterInputController.cs
@@ -5,6 +5,7 @@ public class CharacterInputController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     private InputSystemController _playerInput;
+    [SerializeField] private Animator _animator;
 
     private Vector3 _direction;
 
@@ -25,11 +26,24 @@ public class CharacterInputController : MonoBehaviour
         _direction = _playerInput.Player.Move.ReadValue<Vector3>();
 
         Move(_direction);
+        PlayRunAnimation(_direction);
     }
 
     private void Move(Vector3 direction)
     {
         Debug.Log(direction);
+        PlayRunAnimation(direction);
         transform.Translate(_direction * _moveSpeed * Time.deltaTime);
     }
+    private void PlayRunAnimation(Vector3 direction)
+    {
+        if (direction != new Vector3(0, 0, 0))
+        {
+            _animator.SetBool("IsMoving", true);
+            Debug.Log("wwww");
+        }
+        else
+            _animator.SetBool("IsMoving", false);
+    }
 }
+
